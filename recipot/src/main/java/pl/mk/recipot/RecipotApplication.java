ebb2c68a85e0f730798d6ec668770822c2054b56
Monika Kordoń <37344587+mk1808@users.noreply.github.com>
2023-06-24 21:13:23 +0200

    feature #1: initialize Spring Boot project

---
+++ recipot/src/main/java/pl/mk/recipot/RecipotApplication.java
@@ -0,0 +1,13 @@
+package pl.mk.recipot;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RecipotApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(RecipotApplication.class, args);
+	}
+
+}
---
+++ recipot/src/test/java/pl/mk/recipot/RecipotApplicationTests.java
@@ -0,0 +1,13 @@
+package pl.mk.recipot;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class RecipotApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
