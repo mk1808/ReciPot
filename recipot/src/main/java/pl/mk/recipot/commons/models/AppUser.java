98dfd1f37ac16421b13464dbbe93a6d05dacfd1b
Monika Kordoń <37344587+mk1808@users.noreply.github.com>
2023-06-25 14:39:47 +0200

    feature #2: adding user roles

--- recipot/src/main/java/pl/mk/recipot/commons/models/AppUser.java
+++ recipot/src/main/java/pl/mk/recipot/commons/models/AppUser.java
@@ -1,25 +1,38 @@
 package pl.mk.recipot.commons.models;
 
+import java.util.HashSet;
+import java.util.Set;
 import java.util.UUID;
 
 import org.hibernate.annotations.UuidGenerator;
 import jakarta.validation.constraints.Email;
-
+import jakarta.persistence.CascadeType;
 import jakarta.persistence.Entity;
+import jakarta.persistence.FetchType;
 import jakarta.persistence.GeneratedValue;
 import jakarta.persistence.Id;
+import jakarta.persistence.JoinTable;
+import jakarta.persistence.ManyToMany;
+import jakarta.persistence.JoinColumn;
 
 @Entity
 public class AppUser {
-    @Id
-    @GeneratedValue
-    @UuidGenerator
-    private UUID id;
-    
-    @Email
-    private String email;
-    private String login;
-    private String password;
-    private String role;
-    private Boolean verified;
+	@Id
+	@GeneratedValue
+	@UuidGenerator
+	private UUID id;
+
+	@Email
+	private String email;
+	private String login;
+	private String password;
+	private String role;
+	private Boolean verified;
+
+	@ManyToMany(cascade = CascadeType.ALL, fetch = FetchType.EAGER)
+	@JoinTable(
+			name = "app_users_roles", 
+			joinColumns = @JoinColumn(name = "user_id"), 
+			inverseJoinColumns = @JoinColumn(name = "role_id"))
+	private Set<Role> roles = new HashSet<>();
 }
