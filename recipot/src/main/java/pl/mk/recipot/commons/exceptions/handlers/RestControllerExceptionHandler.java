3bfb2bf41d9c67990c7762c9548c8a6fc0409bd2
Monika Kordoń <37344587+mk1808@users.noreply.github.com>
2023-07-01 14:29:51 +0200

    feature #55: add exception and handler

---
+++ recipot/src/main/java/pl/mk/recipot/commons/exceptions/handlers/RestControllerExceptionHandler.java
@@ -0,0 +1,23 @@
+package pl.mk.recipot.commons.exceptions.handlers;
+
+
+
+import org.springframework.http.HttpHeaders;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.ControllerAdvice;
+import org.springframework.web.bind.annotation.ExceptionHandler;
+import org.springframework.web.context.request.WebRequest;
+import org.springframework.web.servlet.mvc.method.annotation.ResponseEntityExceptionHandler;
+
+import pl.mk.recipot.commons.exceptions.UnauthorizedException;
+
+@ControllerAdvice
+public class RestControllerExceptionHandler extends ResponseEntityExceptionHandler {
+
+    @ExceptionHandler(value = { UnauthorizedException.class })
+    protected ResponseEntity<Object> handleConflict(RuntimeException ex, WebRequest request) {
+        String bodyOfResponse = ex.getMessage();
+        return handleExceptionInternal(ex, bodyOfResponse, new HttpHeaders(), HttpStatus.CONFLICT, request);
+    }
+}
