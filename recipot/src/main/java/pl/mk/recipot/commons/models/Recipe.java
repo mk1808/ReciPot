36a56d433d7f9757d42464c2376a7d8bdc2b5376
Monika Kordoń <37344587+mk1808@users.noreply.github.com>
2023-07-02 21:53:26 +0200

    feature #22: correction

--- recipot/src/main/java/pl/mk/recipot/commons/models/Recipe.java
+++ recipot/src/main/java/pl/mk/recipot/commons/models/Recipe.java
@@ -21,9 +21,7 @@ import jakarta.persistence.ManyToOne;
 import jakarta.persistence.Temporal;
 import jakarta.persistence.TemporalType;
 import jakarta.persistence.Transient;
-import lombok.AllArgsConstructor;
 import lombok.Data;
-import lombok.NoArgsConstructor;
 import pl.mk.recipot.commons.enums.RecipeAccessType;
 import pl.mk.recipot.commons.enums.RecipeAmountOfDishes;
 import pl.mk.recipot.commons.enums.RecipeDifficulty;
@@ -31,8 +29,6 @@ import pl.mk.recipot.commons.enums.RecipeRequiredEffort;
 
 @Entity
 @Data
-@NoArgsConstructor
-@AllArgsConstructor
 public class Recipe {
 	@Id
 	@GeneratedValue
