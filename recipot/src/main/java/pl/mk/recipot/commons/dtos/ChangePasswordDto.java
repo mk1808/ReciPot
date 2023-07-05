0f0eb30755ac307fcb331f859bc07e95e7e981a1
Marek Czopor <marqos111222@gmail.com>
2023-07-05 22:26:11 +0200

    refactor #100: add validators to entities

--- recipot/src/main/java/pl/mk/recipot/commons/dtos/ChangePasswordDto.java
+++ recipot/src/main/java/pl/mk/recipot/commons/dtos/ChangePasswordDto.java
@@ -2,12 +2,17 @@ package pl.mk.recipot.commons.dtos;
 
 import java.util.UUID;
 
+import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.NotNull;
 import jakarta.validation.constraints.Pattern;
 
 public class ChangePasswordDto {
+	@NotBlank(message = "User password is required")
 	@Pattern(regexp = "^(?=.*[a-z])(?=.*[A-Z])(?=.*[0-9])(?=.*[!@#$%^&*])[a-zA-Z0-9!@#$%^&*]{8,30}$", message = "Password to weak")
 	public String password;
+	@NotBlank(message = "User matchingPassword is required")
 	public String matchingPassword;
+	@NotNull(message = "User id is required")
 	public UUID userId;
 
 }
--- recipot/src/main/java/pl/mk/recipot/commons/dtos/UserRegisterDto.java
+++ recipot/src/main/java/pl/mk/recipot/commons/dtos/UserRegisterDto.java
@@ -1,9 +1,17 @@
 package pl.mk.recipot.commons.dtos;
 
+import jakarta.validation.constraints.Email;
+import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.Pattern;
+
 public class UserRegisterDto {
-	
+	@Email(message = "User email is not valid")
 	public String email;
+	@NotBlank(message = "User login is required")
 	public String login;
+	@NotBlank(message = "User password is required")
+	@Pattern(regexp = "^(?=.*[a-z])(?=.*[A-Z])(?=.*[0-9])(?=.*[!@#$%^&*])[a-zA-Z0-9!@#$%^&*]{8,30}$", message = "Password to weak")
 	public String password;
+	@NotBlank(message = "User matchingPassword is required")
 	public String matchingPassword;
 }
