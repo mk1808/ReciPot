039944ae0285abe36dae3704e364d5e3fb5f6626
Marek Czopor <marqos111222@gmail.com>
2023-07-05 22:27:36 +0200

    refactor #100: add validators to entities

--- recipot/src/main/java/pl/mk/recipot/commons/dtos/UserLoginDto.java
+++ recipot/src/main/java/pl/mk/recipot/commons/dtos/UserLoginDto.java
@@ -1,5 +1,6 @@
 package pl.mk.recipot.commons.dtos;
 
+import jakarta.validation.constraints.NotBlank;
 import lombok.AllArgsConstructor;
 import lombok.Data;
 import lombok.NoArgsConstructor;
@@ -8,6 +9,8 @@ import lombok.NoArgsConstructor;
 @NoArgsConstructor
 @Data
 public class UserLoginDto {
+	@NotBlank(message = "User login is required")
 	public String username; 
+	@NotBlank(message = "User password is required")
 	public String password; 
 }
