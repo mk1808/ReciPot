eeacf5d6045e9882b60b5f64e21574b3edc9010f
Monika Kordoń <37344587+mk1808@users.noreply.github.com>
2023-07-05 17:51:10 +0200

    feature #27: after review - refactor

--- recipot/src/main/java/pl/mk/recipot/recipes/domains/UpdateExistingIngredients.java
+++ recipot/src/main/java/pl/mk/recipot/recipes/domains/UpdateExistingIngredients.java
@@ -1,37 +1,31 @@
 package pl.mk.recipot.recipes.domains;
 
-import java.util.ArrayList;
 import java.util.Comparator;
-import java.util.Iterator;
 import java.util.List;
-import java.util.Map;
-import java.util.Set;
 import java.util.UUID;
-import java.util.stream.Collectors;
-import java.util.stream.Stream;
 
-import org.springframework.data.util.Pair;
 import org.springframework.data.util.StreamUtils;
 
-import pl.mk.recipot.commons.models.Ingredient;
 import pl.mk.recipot.commons.models.Recipe;
 import pl.mk.recipot.commons.models.RecipeIngredient;
 
 public class UpdateExistingIngredients {
 	
-		public List<RecipeIngredient> execute(List<RecipeIngredient> recipeIngredientsToUpdate, 
-				List<RecipeIngredient> fromRecipe, Recipe recipe){
-			List<UUID> idsToUpdate = getIdsToUpdate(recipeIngredientsToUpdate);
-			List<RecipeIngredient> fromRecipeFiltered = fromRecipe.stream()
-					.filter(r->idsToUpdate.contains(r.getId())).toList();
-			 Stream<RecipeIngredient> stream = StreamUtils.zip(
-					 recipeIngredientsToUpdate.stream().sorted(sort()),
-					 fromRecipeFiltered.stream().sorted(sort()),
-					  (ri1, ri2) -> updateSingle(ri1, ri2));
-			 List<RecipeIngredient> toSave = stream.toList();
-			 
-			 return toSave;
-		}
+	public List<RecipeIngredient> execute(List<RecipeIngredient> recipeIngredientsToUpdate,
+			List<RecipeIngredient> fromRecipe, Recipe recipe) {
+		List<UUID> idsToUpdate = getIdsToUpdate(recipeIngredientsToUpdate);
+		List<RecipeIngredient> fromRecipeFiltered = fromRecipe.stream().filter(r -> idsToUpdate.contains(r.getId()))
+				.toList();
+
+		return StreamUtils
+				.zip(
+						recipeIngredientsToUpdate.stream().sorted(sort()),
+						fromRecipeFiltered.stream().sorted(sort()), 
+						this::updateSingle
+				)
+				.toList();
+
+	}
 		
 		private Comparator<? super RecipeIngredient> sort() {
 			return Comparator.comparing(RecipeIngredient::getId);
