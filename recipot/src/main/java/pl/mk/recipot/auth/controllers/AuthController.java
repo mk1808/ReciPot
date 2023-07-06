be8c51434d2b9f5172189f4710513f2cfba2aa3a
Marek Czopor <marqos111222@gmail.com>
2023-07-06 17:21:49 +0200

    refacotr #103: standarize controllers annotations

--- recipot/src/main/java/pl/mk/recipot/auth/controllers/AuthController.java
+++ recipot/src/main/java/pl/mk/recipot/auth/controllers/AuthController.java
@@ -1,23 +1,19 @@
 package pl.mk.recipot.auth.controllers;
 
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 import jakarta.servlet.http.HttpServletRequest;
-import jakarta.validation.Valid;
 import pl.mk.recipot.auth.services.IAuthService;
 import pl.mk.recipot.commons.dtos.ChangePasswordDto;
 import pl.mk.recipot.commons.dtos.Response;
 import pl.mk.recipot.commons.dtos.UserRegisterDto;
 import pl.mk.recipot.commons.factories.OkMessageResponseFactory;
-import pl.mk.recipot.commons.factories.OkResponseFactory;
 import pl.mk.recipot.commons.models.AppUser;
 
 @RestController
 public class AuthController implements IAuthController {
-	
+
 	private IAuthService authService;
 
 	public AuthController(IAuthService authService) {
@@ -26,12 +22,12 @@ public class AuthController implements IAuthController {
 	}
 
 	@Override
-	public AppUser register(@RequestBody @Valid UserRegisterDto userRegisterDto, HttpServletRequest request) {
+	public AppUser register(UserRegisterDto userRegisterDto, HttpServletRequest request) {
 		return authService.register(userRegisterDto);
 	}
 
 	@Override
-	public ResponseEntity<Response<Void>> changePassword(@RequestBody @Valid ChangePasswordDto changePasswordDto) {
+	public ResponseEntity<Response<Void>> changePassword(ChangePasswordDto changePasswordDto) {
 		authService.changePassword(changePasswordDto);
 		return new OkMessageResponseFactory().createResponse("auth.success.passwordChanged");
 	}
--- recipot/src/main/java/pl/mk/recipot/auth/controllers/IAuthController.java
+++ recipot/src/main/java/pl/mk/recipot/auth/controllers/IAuthController.java
@@ -3,10 +3,12 @@ package pl.mk.recipot.auth.controllers;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.PatchMapping;
 import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 import jakarta.servlet.http.HttpServletRequest;
+import jakarta.validation.Valid;
 import pl.mk.recipot.commons.dtos.ChangePasswordDto;
 import pl.mk.recipot.commons.dtos.Response;
 import pl.mk.recipot.commons.dtos.UserRegisterDto;
@@ -17,9 +19,9 @@ import pl.mk.recipot.commons.models.AppUser;
 public interface IAuthController {
 	
 	@PostMapping("/register")
-	AppUser register(UserRegisterDto userRegisterDto, HttpServletRequest request);
+	AppUser register(@RequestBody @Valid UserRegisterDto userRegisterDto, HttpServletRequest request);
 	
 	@PatchMapping("/changePassword")
-	ResponseEntity<Response<Void>> changePassword(ChangePasswordDto changePasswordDto);
+	ResponseEntity<Response<Void>> changePassword(@RequestBody @Valid ChangePasswordDto changePasswordDto);
 
 }
