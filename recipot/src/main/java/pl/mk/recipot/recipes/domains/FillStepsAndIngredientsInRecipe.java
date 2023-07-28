dbc9d5f478198be1da25073c65eaf4d969ed1ad5
Monika Kordoń <37344587+mk1808@users.noreply.github.com>
2023-07-28 20:00:10 +0200

    bug #130: return ingredients and steps in recipe, correct recipe save

---
+++ recipot/src/main/java/pl/mk/recipot/recipes/domains/FillStepsAndIngredientsInRecipe.java
@@ -0,0 +1,16 @@
+package pl.mk.recipot.recipes.domains;
+
+import java.util.List;
+
+import pl.mk.recipot.commons.models.Recipe;
+import pl.mk.recipot.commons.models.RecipeIngredient;
+import pl.mk.recipot.commons.models.RecipeStep;
+
+public class FillStepsAndIngredientsInRecipe {
+	public Recipe execute(Recipe recipe, List<RecipeIngredient> ingredients, List<RecipeStep> steps) {
+		recipe.setRecipeSteps(steps);
+		recipe.setRecipeIngredients(ingredients);
+		return recipe;
+
+	}
+}
