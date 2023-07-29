d738fed65b110433e495c1105fc4c57e20a1a45c
Monika Kordoń <37344587+mk1808@users.noreply.github.com>
2023-07-29 21:30:59 +0200

    refactor #145: use new classes for recipe

--- recipot/src/main/java/pl/mk/recipot/recipes/domains/FillRecipeWithIngredients.java
+++ recipot/src/main/java/pl/mk/recipot/recipes/domains/FillRecipeWithIngredients.java
@@ -2,13 +2,14 @@ package pl.mk.recipot.recipes.domains;
 
 import java.util.List;
 
+import pl.mk.recipot.commons.domains.SetRecipeNull;
 import pl.mk.recipot.commons.models.Recipe;
 import pl.mk.recipot.commons.models.RecipeIngredient;
 
 public class FillRecipeWithIngredients {
 	public Recipe execute(Recipe recipe, List<RecipeIngredient> added, List<RecipeIngredient> updated) {
 		added.addAll(updated);
-		added.stream().forEach(ri -> ri.setRecipe(null));
+		added.stream().forEach(new SetRecipeNull()::execute);
 		recipe.setRecipeIngredients(added);
 		return recipe;
 
