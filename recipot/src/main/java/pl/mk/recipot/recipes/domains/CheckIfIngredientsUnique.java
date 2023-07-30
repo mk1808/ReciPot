9b7c47ec21e087aa2d9354bea07799b22229ac83
Marek Czopor <marqos111222@gmail.com>
2023-07-30 13:33:42 +0200

    refactor #152: reused getIngredientsName

--- recipot/src/main/java/pl/mk/recipot/recipes/domains/CheckIfIngredientsUnique.java
+++ recipot/src/main/java/pl/mk/recipot/recipes/domains/CheckIfIngredientsUnique.java
@@ -7,7 +7,8 @@ import pl.mk.recipot.commons.models.Ingredient;
 
 public class CheckIfIngredientsUnique {
 	public void execute(List<Ingredient> ingredients) {
-		List<String> uniqueNames = ingredients.stream().map(Ingredient::getName).distinct().toList();
+		List<String> ingredientsNames = new GetRecipeIngredientsNames().execute(ingredients);
+		List<String> uniqueNames = ingredientsNames.stream().distinct().toList();
 		if (uniqueNames.size() != ingredients.size()) {
 			throw new BadRequestException("recipes.error.ingredientsNotUnique");
 		}
