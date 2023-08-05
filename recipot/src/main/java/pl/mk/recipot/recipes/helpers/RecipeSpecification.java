c14af45dff2a13c2ac2478ccd50cb4ada80e14c8
Monika Kordoń <37344587+mk1808@users.noreply.github.com>
2023-08-06 00:04:35 +0200

    feature #18: adding different types of operations

--- recipot/src/main/java/pl/mk/recipot/recipes/helpers/RecipeSpecification.java
+++ recipot/src/main/java/pl/mk/recipot/recipes/helpers/RecipeSpecification.java
@@ -29,15 +29,72 @@ public class RecipeSpecification implements Specification<Recipe> {
 		String strToSearch = searchCriteria.getValue().toString().toLowerCase();
 
 		switch (Objects.requireNonNull(SearchOperation.getSimpleOperation(searchCriteria.getOperation()))) {
-		case CONTAINS: {
-			if (searchCriteria.getFilterKey().equals("a")) {
-				return criteriaBuilder.like(
-						criteriaBuilder.lower(userJoin(root).<String>get(searchCriteria.getFilterKey())),
-						"%" + strToSearch + "%");
-			}
-			return criteriaBuilder.like(criteriaBuilder.lower(root.get(searchCriteria.getFilterKey())),
-					"%" + strToSearch + "%");
-		}
+		case CONTAINS:
+            if(searchCriteria.getFilterKey().equals("a")){
+                return criteriaBuilder.like(criteriaBuilder.lower(userJoin(root).<String>get(searchCriteria.getFilterKey())), "%" + strToSearch + "%");
+            }
+            return criteriaBuilder.like(criteriaBuilder.lower(root.get(searchCriteria.getFilterKey())), "%" + strToSearch + "%");
+
+        case DOES_NOT_CONTAIN:
+            if(searchCriteria.getFilterKey().equals("deptName")){
+                return criteriaBuilder.notLike(criteriaBuilder.lower(userJoin(root).<String>get(searchCriteria.getFilterKey())), "%" + strToSearch + "%");
+            }
+            return criteriaBuilder.notLike(criteriaBuilder.lower(root.get(searchCriteria.getFilterKey())), "%" + strToSearch + "%");
+
+        case BEGINS_WITH:
+            if(searchCriteria.getFilterKey().equals("deptName")){
+                return criteriaBuilder.like(criteriaBuilder.lower(userJoin(root).<String>get(searchCriteria.getFilterKey())), strToSearch + "%");
+            }
+            return criteriaBuilder.like(criteriaBuilder.lower(root.get(searchCriteria.getFilterKey())), strToSearch + "%");
+
+        case DOES_NOT_BEGIN_WITH:
+            if(searchCriteria.getFilterKey().equals("deptName")){
+                return criteriaBuilder.notLike(criteriaBuilder.lower(userJoin(root).<String>get(searchCriteria.getFilterKey())), strToSearch + "%");
+            }
+            return criteriaBuilder.notLike(criteriaBuilder.lower(root.get(searchCriteria.getFilterKey())), strToSearch + "%");
+
+        case ENDS_WITH:
+            if(searchCriteria.getFilterKey().equals("deptName")){
+                return criteriaBuilder.like(criteriaBuilder.lower(userJoin(root).<String>get(searchCriteria.getFilterKey())), "%" + strToSearch);
+            }
+            return criteriaBuilder.like(criteriaBuilder.lower(root.get(searchCriteria.getFilterKey())), "%" + strToSearch);
+
+        case DOES_NOT_END_WITH:
+            if(searchCriteria.getFilterKey().equals("deptName")){
+                return criteriaBuilder.notLike(criteriaBuilder.lower(userJoin(root).<String>get(searchCriteria.getFilterKey())), "%" + strToSearch);
+            }
+            return criteriaBuilder.notLike(criteriaBuilder.lower(root.get(searchCriteria.getFilterKey())), "%" + strToSearch);
+
+        case EQUAL:
+            if(searchCriteria.getFilterKey().equals("deptName")){
+                System.out.println(searchCriteria.getValue());
+                return criteriaBuilder.equal(userJoin(root).<String>get(searchCriteria.getFilterKey()), searchCriteria.getValue());
+            }
+            return criteriaBuilder.equal(root.get(searchCriteria.getFilterKey()), searchCriteria.getValue());
+
+        case NOT_EQUAL:
+            if(searchCriteria.getFilterKey().equals("deptName")){
+                return criteriaBuilder.notEqual(userJoin(root).<String>get(searchCriteria.getFilterKey()), searchCriteria.getValue() );
+            }
+            return criteriaBuilder.notEqual(root.get(searchCriteria.getFilterKey()), searchCriteria.getValue());
+
+        case NUL:
+            return criteriaBuilder.isNull(root.get(searchCriteria.getFilterKey()));
+
+        case NOT_NULL:
+            return criteriaBuilder.isNotNull(root.get(searchCriteria.getFilterKey()));
+
+        case GREATER_THAN:
+            return criteriaBuilder.greaterThan(root.<String> get(searchCriteria.getFilterKey()), searchCriteria.getValue().toString());
+
+        case GREATER_THAN_EQUAL:
+            return criteriaBuilder.greaterThanOrEqualTo(root.<String> get(searchCriteria.getFilterKey()), searchCriteria.getValue().toString());
+
+        case LESS_THAN:
+            return criteriaBuilder.lessThan(root.<String> get(searchCriteria.getFilterKey()), searchCriteria.getValue().toString());
+
+        case LESS_THAN_EQUAL:
+            return criteriaBuilder.lessThanOrEqualTo(root.<String> get(searchCriteria.getFilterKey()), searchCriteria.getValue().toString());
 		}
 
 		return null;
