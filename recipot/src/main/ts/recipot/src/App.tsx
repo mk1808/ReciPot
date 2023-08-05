46b1163e6c917fcc42f55ac293d7888d3737eaaa
Monika Kordoń <37344587+mk1808@users.noreply.github.com>
2023-08-05 10:50:37 +0200

    feature #174: adding basic routing and components

--- recipot/src/main/ts/recipot/src/App.tsx
+++ recipot/src/main/ts/recipot/src/App.tsx
@@ -1,21 +1,33 @@
 import React, { useEffect } from 'react';
+import { Routes, Route, Navigate } from 'react-router-dom';
 import logo from './logo.svg';
 import './App.scss';
 import Button from 'react-bootstrap/Button';
 import restClient from './api/RestClient';
 import recipesApi from './api/RecipesApi';
 import authApi from './api/AuthApi';
+import Main from './pages/main/Main';
+import Login from './pages/auth/login/Login';
+import Register from './pages/auth/register/Register';
+import NotFound from './pages/other/notFound/NotFound';
+import RecipeDetails from './pages/recipe/details/RecipeDetails';
+import RecipeAdd from './pages/recipe/add/RecipeAdd';
+
 
 function App() {
 
-  useEffect(()=>{
-    authApi.login({username:"randomuser1234567",password:"Password1!"}, ()=>{}, ()=>{})
-   
-  },[])
+  useEffect(() => {
+    authApi.login({ username: "randomuser1234567", password: "Password1!" }, () => { }, () => { })
 
-  const send=()=>{
- recipesApi.getRecipe('14e1ea5f-b236-4f4a-b3ab-cdc6b7b93562', ()=>{})
+  }, [])
+
+  const send = () => {
+    recipesApi.getRecipe('14e1ea5f-b236-4f4a-b3ab-cdc6b7b93562', () => { })
   }
+  const hasAccess=true;
+
+
+
 
   return (
     <div className="App">
@@ -30,15 +42,26 @@ function App() {
           target="_blank"
           rel="noopener noreferrer"
         >
-    
- 
+
+
           Learn React
-         
+
         </a>
         <Button variant="primary" onClick={send}>Primary</Button>{' '}
       </header>
+      <Routes>
+        <Route path="/" element={<Main />} />
+        <Route path="/login" element={<Login />} />
+        <Route path="/register" element={<Register />} />
+        <Route path="/recipes/add" element={<RecipeAdd />} />
+        <Route path="/recipes/:id" element={<RecipeDetails />} />
+        <Route path="*" element={<NotFound />} />
+      </Routes>
+
+
+
+
     </div>
   );
 }
-   /*  {RestClient()}*/
 export default App;
---
+++ recipot/src/main/ts/recipot/src/pages/recipe/details/RecipeDetails.tsx
@@ -0,0 +1,9 @@
+function RecipeDetails() {
+    return (
+        <h1>RecipeDetails</h1>
+    );
+
+
+}
+
+export default RecipeDetails;
