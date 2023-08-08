11e47c95c4a62541f46ed49ec5cd81154a87d7a5
Monika Kordoń <37344587+mk1808@users.noreply.github.com>
2023-08-08 21:49:39 +0200

    feature #188: recipe steps - adding working onscroll

--- recipot/src/main/ts/recipot/src/pages/other/test/Test.tsx
+++ recipot/src/main/ts/recipot/src/pages/other/test/Test.tsx
@@ -29,29 +29,9 @@ function Test() {
         description: "Some quick example text to build on the card title and make up the bulk of the card's content.",
         photo: "data:image/svg+xml;charset=UTF-8,%3Csvg%20width%3D%22286%22%20height%3D%22180%22%20xmlns%3D%22http%3A%2F%2Fwww.w3.org%2F2000%2Fsvg%22%20viewBox%3D%220%200%20286%20180%22%20preserveAspectRatio%3D%22none%22%3E%3Cdefs%3E%3Cstyle%20type%3D%22text%2Fcss%22%3E%23holder_189cc491e6b%20text%20%7B%20fill%3A%23999%3Bfont-weight%3Anormal%3Bfont-family%3AArial%2C%20Helvetica%2C%20Open%20Sans%2C%20sans-serif%2C%20monospace%3Bfont-size%3A14pt%20%7D%20%3C%2Fstyle%3E%3C%2Fdefs%3E%3Cg%20id%3D%22holder_189cc491e6b%22%3E%3Crect%20width%3D%22286%22%20height%3D%22180%22%20fill%3D%22%23373940%22%3E%3C%2Frect%3E%3Cg%3E%3Ctext%20x%3D%22107.1953125%22%20y%3D%2296.3%22%3E286x180%3C%2Ftext%3E%3C%2Fg%3E%3C%2Fg%3E%3C%2Fsvg%3E"
     };
-    const recipeCallback = () => { console.log("go") }
+    const recipeCallback = () => {  }
     const [show, setShow] = useState(false);
-    const [scrollTop, setScrollTop] = useState(0);
-    useEffect(() => {
-        const handleScroll = (event: any) => {
-            setScrollTop(window.scrollY);
-        };
-        window.addEventListener('scroll', handleScroll);
-        return () => {
-            window.removeEventListener('scroll', handleScroll);
-        };
 
-    }, [])
-    function getColor(): string {
-        let name = "my-block",
-            element = document.getElementById(name), //ref
-            offsetTop = element?.offsetTop || 0,
-            scrollTop = window?.scrollY || 0;
-        console.log("offset " + offsetTop)
-        console.log("scrl " + scrollTop)
-        console.log(offsetTop - scrollTop)
-        return (offsetTop - scrollTop) < 600 ? "black" : "red";
-    }
 
     return (<>
         <h1>Test</h1>
@@ -71,7 +51,7 @@ function Test() {
                 <RecipeCard className="mt-5" recipe={recipe} recipeCallback={recipeCallback}></RecipeCard >
                 <Form.Check></Form.Check>
 
-                <div id="my-block" style={{ height: 20, width: 600, backgroundColor: getColor() }}></div>
+                
                 <button onClick={() => { setShow(!show); }}>showme</button>
                 <div className="alert-container">
                     {show && <MyAlert.Primary >This is a primary alert—check it out!</MyAlert.Primary>}
