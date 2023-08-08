f63f020724a81597082e141225319f3dd6617d89
Monika Kordoń <37344587+mk1808@users.noreply.github.com>
2023-08-09 01:46:49 +0200

    feature #188: sliding cards continuation

--- recipot/src/main/ts/recipot/src/components/complex/SlidingCards.tsx
+++ recipot/src/main/ts/recipot/src/components/complex/SlidingCards.tsx
@@ -1,62 +1,54 @@
 import React, { useState, useEffect } from "react";
+import { Stack } from "react-bootstrap";
+import MyButton from "../basicUi/MyButton";
 
 function SlidingCards() {
-    const [show, toggleShow] = useState(true);
+    const SLIDER_SIZE = 3;
     const [counter, setCounter] = useState(0);
-    
-
     const [slicedTab, setSlicedTab] = useState<any[] | undefined>([]);
-    function getRandomInt(max?:number|undefined) {
-        return Math.floor(Math.random() * 100);
-      }
 
     const tab: any = ["card1", "card2", "card3", "card4", "card5", "card6", "card7"];
     const photos1: any = [
         `https://picsum.photos/id/1/200/300`,
-    `https://picsum.photos/id/5/200/300`,
-    `https://picsum.photos/id/10/200/300`,
-    `https://picsum.photos/id/20/200/300`,
-    `https://picsum.photos/id/40/200/300`,
-    `https://picsum.photos/id/55/200/300`,
-    `https://picsum.photos/id/66/200/300` ];
+        `https://picsum.photos/id/5/200/300`,
+        `https://picsum.photos/id/10/200/300`,
+        `https://picsum.photos/id/20/200/300`,
+        `https://picsum.photos/id/40/200/300`,
+        `https://picsum.photos/id/55/200/300`,
+        `https://picsum.photos/id/66/200/300`];
     const [photos, setPhotos] = useState(photos1);
-    const sliceT = () => {
+    const sliceTab = () => {
         if (counter > -1 && counter < 5) {
-            var sliced = [...tab].slice(counter, counter + 3);
+            var sliced = [...tab].slice(counter, counter + SLIDER_SIZE);
             setSlicedTab(sliced);
-
-            var slicedP = [...photos1].slice(counter, counter + 3);
+            var slicedP = [...photos1].slice(counter, counter + SLIDER_SIZE);
             setPhotos(slicedP);
         }
-
     };
+    const clickSlide = (counterValue: number) => {
+        setCounter(counterValue);
+        sliceTab();
+    }
+
     useEffect(() => {
-        sliceT();
+        sliceTab();
     }, [counter]);
     return (
         <>
-            <div className="main-div">
-                <button
-                    onClick={() => {
-                        setCounter(counter == 0 ? 0 : counter - 1);
-                        sliceT();
-                    }}
-                >
+            <Stack direction="horizontal" >
+                <MyButton.Primary onClick={() => clickSlide(counter == 0 ? 0 : counter - 1)}>
                     &#10094;
-                </button>
-                {slicedTab?.length} c:{counter}
+                </MyButton.Primary>
+                
                 {slicedTab?.map((element, index) => {
-                    return <div className="width m-3" key={element}><h3>{element}</h3><img src={photos[index]}/></div>;
+                    return <div className="width m-2" key={element}><h3>{element}</h3><img src={photos[index]} /></div>;
                 })}
-                <button
-                    onClick={() => {
-                        setCounter(counter == 4 ? 4 : counter + 1);
-                        sliceT();
-                    }}
-                >
+
+                <MyButton.Primary onClick={() => clickSlide(counter == 4 ? 4 : counter + 1)}>
                     &#10095;
-                </button>
-            </div>
+                </MyButton.Primary>
+
+            </Stack>
         </>
     );
 
