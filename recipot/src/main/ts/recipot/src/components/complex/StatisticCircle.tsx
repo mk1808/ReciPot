6883f079c454f63c127e7060be737b62a45670b4
Marek Czopor <marqos111222@gmail.com>
2023-08-08 22:16:03 +0200

    feature #188: add statistic circle component

--- recipot/src/main/ts/recipot/src/components/complex/StatisticCircle.tsx
+++ recipot/src/main/ts/recipot/src/components/complex/StatisticCircle.tsx
@@ -1,29 +1,37 @@
-import { Stack } from "react-bootstrap";
 
 function StatisticCircle({ value = '', label = '', size = 100, ringSize = 30, mainColor = "rgb(13, 110, 253)", secondaryColor = "#fff" }) {
 
-    return <Stack>
-        <span className="statistic-circle-label">{label}</span>
+    return <div>
         {renderRing()}
-    </Stack>
+        <span className="statistic-circle-label">{label}</span>
+    </div>
 
     function renderRing() {
-        return <div className="statistic-circle" style={{ width: size, height: size }}>
+        return <div className="statistic-circle mx-auto" style={{ width: size, height: size }}>
             {renderHalfRing("main")}
             {renderHalfRing("rotated")}
-            <span className="statistic-circle-value" >{value}</span>
+            {renderValue()}
         </div>
     }
 
     function renderHalfRing(className: string) {
         return <div className={className} style={{ width: size, height: size }}>
             {renderHalfCircle(size, mainColor)}
-            {renderHalfCircle(size - ringSize, secondaryColor)}
+            {renderCircle(size - ringSize, secondaryColor)}
         </div>
+    }
+    function renderCircle(diameter: number, backgroundColor: string) {
+        return <div className="circle" style={{ backgroundColor, width: diameter, height: diameter, margin: (size - diameter) / 2 }}></div>
     }
 
     function renderHalfCircle(diameter: number, backgroundColor: string) {
         return <div className="half-circle" style={{ backgroundColor, width: diameter, height: diameter, margin: (size - diameter) / 2 }}></div>
+    }
+
+    function renderValue() {
+        return <div style={{ width: size, height: size }} className="statistic-circle-value" >
+            <span>{value}</span>
+        </div>
     }
 }
 
