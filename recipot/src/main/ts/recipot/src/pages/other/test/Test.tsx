77d439fcd7dcfde4b881ce6a1e4bb4ddcf1dbfe2
Monika Kordoń <37344587+mk1808@users.noreply.github.com>
2023-08-11 17:51:17 +0200

    refactor #195: myImage component refactor

--- recipot/src/main/ts/recipot/src/pages/other/test/Test.tsx
+++ recipot/src/main/ts/recipot/src/pages/other/test/Test.tsx
@@ -52,6 +52,8 @@ function Test() {
     const handleSubmit = () => { console.log("successfull submit"); setShowModal(false) };
     const dispatch = useContext(AlertsDispatchContext);
     let nextId = 0;
+    const photo="data:image/svg+xml;charset=UTF-8,%3Csvg%20width%3D%22171%22%20height%3D%22180%22%20xmlns%3D%22http%3A%2F%2Fwww.w3.org%2F2000%2Fsvg%22%20viewBox%3D%220%200%20171%20180%22%20preserveAspectRatio%3D%22none%22%3E%3Cdefs%3E%3Cstyle%20type%3D%22text%2Fcss%22%3E%23holder_189d8cc414e%20text%20%7B%20fill%3A%23999%3Bfont-weight%3Anormal%3Bfont-family%3AArial%2C%20Helvetica%2C%20Open%20Sans%2C%20sans-serif%2C%20monospace%3Bfont-size%3A10pt%20%7D%20%3C%2Fstyle%3E%3C%2Fdefs%3E%3Cg%20id%3D%22holder_189d8cc414e%22%3E%3Crect%20width%3D%22171%22%20height%3D%22180%22%20fill%3D%22%23373940%22%3E%3C%2Frect%3E%3Cg%3E%3Ctext%20x%3D%2259.921875%22%20y%3D%2294.5%22%3E171x180%3C%2Ftext%3E%3C%2Fg%3E%3C%2Fg%3E%3C%2Fsvg%3E";
+
 
     const testOptions = [{ label: "op1", value: { name: "nam1" } }, { label: "op2", value: { name: "nam2" } }, { label: "op3", value: { name: "nam3" } }];
 
@@ -108,7 +110,7 @@ function Test() {
                 </div>
                 
                 <div>
-                    <MyImage></MyImage>
+                    <MyImage src={photo} height="300" rounded></MyImage>
                 </div>
                 <div>
                     <MyButton.Primary onClick={handleShow}>
