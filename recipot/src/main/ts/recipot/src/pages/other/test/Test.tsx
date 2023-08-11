fe77fca9b25bd9c7ad3f524205cc78da103abd11
Monika Kordoń <37344587+mk1808@users.noreply.github.com>
2023-08-11 16:54:57 +0200

    refactor #195: import correction

--- recipot/src/main/ts/recipot/src/pages/other/test/Test.tsx
+++ recipot/src/main/ts/recipot/src/pages/other/test/Test.tsx
@@ -18,7 +18,6 @@ import CustomModal from "../../../components/basicUi/CustomModal";
 import RecipeStepsNumbers from "../../../components/complex/RecipeStepsNumbers";
 import SlidingCards from "../../../components/complex/SlidingCards";
 import SlidingElements from "../../../components/complex/SlidingCards";
-import MyImage from "../../../components/basicUi/MyImage";
 import MyInput from "../../../components/basicUi/MyInput";
 import MyTextarea from "../../../components/basicUi/MyTextarea";
 import MyCheckbox from "../../../components/basicUi/MyCheckbox";
