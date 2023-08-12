3a248623db1c9774fd0dada23daf20a5dce06ab5
Marek Czopor <marqos111222@gmail.com>
2023-08-12 20:58:59 +0200

    refactor #195: refactor statistic circle

--- recipot/src/main/ts/recipot/src/components/complex/StatisticCircle.tsx
+++ recipot/src/main/ts/recipot/src/components/complex/StatisticCircle.tsx
@@ -2,22 +2,26 @@ import Stack from 'react-bootstrap/Stack';
 
 function StatisticCircle({ value, label, size = 100, ringSize = 20 }: { value: string, label: string, size?: number, ringSize?: number }) {
 
-    return <Stack className="align-items-center">
-        <div className="statistic-circle">
-            {renderRing()}
-            {renderValue()}
-        </div>
-        <span className="statistic-circle-label">{label}</span>
-    </Stack>
+    return (
+        <Stack className="align-items-center">
+            <div className="statistic-circle">
+                {renderRing()}
+                {renderValue()}
+            </div>
+            <span className="statistic-circle-label">{label}</span>
+        </Stack>
+    );
 
     function renderRing() {
         return <div className="statistic-circle-ring" style={{ width: size, height: size, borderWidth: ringSize }}></div>
     }
 
     function renderValue() {
-        return <div className="statistic-circle-value" style={{ width: size, height: size }}>
-            <span>{value}</span>
-        </div>
+        return (
+            <div className="statistic-circle-value" style={{ width: size, height: size }}>
+                <span>{value}</span>
+            </div>
+        );
     }
 
 }
