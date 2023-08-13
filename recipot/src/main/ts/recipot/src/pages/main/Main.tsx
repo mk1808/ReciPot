3104d67762f8cfb7baf539dd20bc3888c4a30fcf
Monika Kordoń <37344587+mk1808@users.noreply.github.com>
2023-08-13 21:10:17 +0200

    feature #199: adding statistics to layout

--- recipot/src/main/ts/recipot/src/pages/main/Main.tsx
+++ recipot/src/main/ts/recipot/src/pages/main/Main.tsx
@@ -1,28 +1,46 @@
-import { Col, Container, Row } from "react-bootstrap";
+import { Col, Container, Row, Stack } from "react-bootstrap";
 import './styles.scss';
 import MyHeader from "../../components/basicUi/MyHeader";
 import { useTranslation } from "react-i18next";
+import StatisticCircle from "../../components/complex/StatisticCircle";
 
 function Main() {
     const { t } = useTranslation();
     return (
         <div className=" main-page">
             <Col className="main-column">
-                <Row className="recipe-circle-container"></Row>
+                <Row className="recipe-circle-container">
+                    {renderRecipeCardCircle()}
+                </Row>
                 <Row className="recipes-list">
                     <div>
                         <h2 className="my-3 display-3">{t('p.newestRecipes')}</h2>
                     </div>
-                    </Row>
-                <Row className="statistics"><div>statistics</div></Row>
+                </Row>
+                <Row className="statistics">
+                    <div className="my-4">
+                    <Stack direction="horizontal" gap={5} className='flex-wrap justify-content-center py-3'>
+                <div className='col-3'><StatisticCircle value="12" size={150} ringSize={30} label={t('p.recipesCount')} /></div>
+                <div className='col-3'><StatisticCircle value="10" size={150} ringSize={30} label={t('p.usersCount')} /></div>
+                <div className='col-3'><StatisticCircle value="53" size={150} ringSize={30} label={t('p.allRecipeCollectionsCount')} /></div>
+          </Stack>
+
+                    </div>
+                </Row>
                 <Row className="recipes-list">
                     <div>
                         <h2 className="my-3 display-3">{t('p.categories')}</h2>
                     </div>
-                    </Row>
+                </Row>
             </Col>
         </div>
     );
+
+    function renderRecipeCardCircle() {
+        return (
+            <></>
+        )
+    }
 
 
 }
