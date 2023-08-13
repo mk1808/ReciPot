fe292b2f389569035f7d72f9777f42316898031a
Monika Kordoń <37344587+mk1808@users.noreply.github.com>
2023-08-13 11:36:52 +0200

    feature #200: register page

--- recipot/src/main/ts/recipot/src/pages/auth/register/Register.tsx
+++ recipot/src/main/ts/recipot/src/pages/auth/register/Register.tsx
@@ -1,9 +1,28 @@
+import { Stack } from "react-bootstrap";
+import { useTranslation } from "react-i18next";
+import MyHeader from "../../../components/basicUi/MyHeader";
+
 function Register() {
+    const { t } = useTranslation();
     return (
-        <h1>REGISTER</h1>
+        <Stack className="justify-content-center py-5 register-page full-height-page" direction="horizontal">
+            <div className="p-4 mb-2 basic-container">
+                <MyHeader title={t('p.registerHeader')}></MyHeader>
+                {renderForm()}
+            </div>
+        </Stack>
     );
 
-
+    function renderForm() {
+        return (
+            <div>
+                <h6 className="display-6">Wpisz dane poniżej, aby założyć konto.</h6>
+                <input type="text" className="my-2"></input> <br></br>
+                <input type="text" className="my-2"></input>  <br></br>
+                <button className="my-2">somebutton</button>  <br></br>
+            </div>
+        )
+    }
 }
 
 export default Register;
