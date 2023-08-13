4c683d2406c63f7f811395eb7673becf4ca8777f
Monika Kordoń <37344587+mk1808@users.noreply.github.com>
2023-08-13 15:27:27 +0200

    feature #203: basic page layout

--- recipot/src/main/ts/recipot/src/pages/recipe/add/RecipeAdd.tsx
+++ recipot/src/main/ts/recipot/src/pages/recipe/add/RecipeAdd.tsx
@@ -1,9 +1,22 @@
+import { Stack } from "react-bootstrap";
+import { useTranslation } from "react-i18next";
+import MyHeader from "../../../components/basicUi/MyHeader";
+
 function RecipeAdd() {
+    const { t } = useTranslation();
     return (
-        <h1>RecipeAdd</h1>
+        <Stack className="justify-content-center py-5 recipe-add-page full-height-page" direction="horizontal">
+            <div className="p-4 mb-2 basic-container-large basic-container-border">
+                <MyHeader title={t('p.newRecipeHeader')}></MyHeader>
+                {renderForm()}
+            </div>
+        </Stack>
     );
 
+    function renderForm() {
+        return (<></>);
 
+    }
 }
 
 export default RecipeAdd;
