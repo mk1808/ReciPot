ea8f400da9a353ff985ea2172b0b5e0fd2c1da26
Monika Kordoń <37344587+mk1808@users.noreply.github.com>
2023-08-13 11:37:07 +0200

    feature #200: login page - refactor

--- recipot/src/main/ts/recipot/src/pages/auth/login/Login.tsx
+++ recipot/src/main/ts/recipot/src/pages/auth/login/Login.tsx
@@ -9,16 +9,21 @@ function Login() {
         <Stack className="justify-content-center py-5 login-page full-height-page" direction="horizontal">
             <div className="p-4 mb-2 basic-container">
                 <MyHeader title={t('p.loginHeader')}></MyHeader>
-                
+                {renderForm()}
+            </div>
+        </Stack>
+    );
+
+    function renderForm() {
+        return (
+            <div>
                 <h6 className="display-6">Wpisz dane poniżej, aby się zalogować.</h6>
                 <input type="text" className="my-2"></input> <br></br>
                 <input type="text" className="my-2"></input>  <br></br>
                 <button className="my-2">somebutton</button>  <br></br>
             </div>
-        </Stack>
-    );
-
-
+        )
+    }
 }
 
 export default Login;
