146e6a08ae0b1e8ddf194399f1cabad1b77e60d7
Monika Kordoń <37344587+mk1808@users.noreply.github.com>
2023-08-13 20:45:38 +0200

    feature #199: main page basic layout

--- recipot/src/main/ts/recipot/src/pages/main/Main.tsx
+++ recipot/src/main/ts/recipot/src/pages/main/Main.tsx
@@ -1,6 +1,27 @@
+import { Col, Container, Row } from "react-bootstrap";
+import './styles.scss';
+import MyHeader from "../../components/basicUi/MyHeader";
+import { useTranslation } from "react-i18next";
+
 function Main() {
+    const { t } = useTranslation();
     return (
-        <h1>Main</h1>
+        <div className=" main-page">
+            <Col className="main-column">
+                <Row className="recipe-circle-container"></Row>
+                <Row className="recipes-list">
+                    <div>
+                        <h2 className="my-3 display-3">{t('p.newestRecipes')}</h2>
+                    </div>
+                    </Row>
+                <Row className="statistics"><div>statistics</div></Row>
+                <Row className="recipes-list">
+                    <div>
+                        <h2 className="my-3 display-3">{t('p.categories')}</h2>
+                    </div>
+                    </Row>
+            </Col>
+        </div>
     );
 
 
