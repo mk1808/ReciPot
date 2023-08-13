0d523285da41253cd3c3ad3ef78fb7e877f2e6e0
Monika Kordoń <37344587+mk1808@users.noreply.github.com>
2023-08-13 11:07:33 +0200

    feature #200: basic login page layout

--- recipot/src/main/ts/recipot/src/pages/auth/login/Login.tsx
+++ recipot/src/main/ts/recipot/src/pages/auth/login/Login.tsx
@@ -1,9 +1,29 @@
+import { Stack } from 'react-bootstrap';
+import './styles.scss';
+import MyHeader from '../../../components/basicUi/MyHeader';
+import { useTranslation } from 'react-i18next';
+
 function Login() {
+    const { t } = useTranslation();
     return (
-        <h1>LOGIN</h1>
+
+        <Stack className=" justify-content-center login-page py-5" direction="horizontal">
+            <div className="p-4 mb-2  test one">
+                <MyHeader title={t('p.login')}></MyHeader>
+                
+                <h6 className="display-6">Wpisz dane poniżej, aby się zalogować.</h6>
+                <input type="text" className="my-2"></input> <br></br>
+                <input type="text" className="my-2"></input>  <br></br>
+                <button className="my-2">somebutton</button>  <br></br>
+            </div>
+        </Stack>
+
+
     );
 
 
 }
 
 export default Login;
+
+//style od ramki do app.scss
