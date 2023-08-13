01c942aa0d2151c4f14714333e6ab9bdc3b90fd7
Monika Kordoń <37344587+mk1808@users.noreply.github.com>
2023-08-13 11:31:00 +0200

    feature #200: login page - refactor, extract common css classes

--- recipot/src/main/ts/recipot/src/pages/auth/login/Login.tsx
+++ recipot/src/main/ts/recipot/src/pages/auth/login/Login.tsx
@@ -6,10 +6,9 @@ import { useTranslation } from 'react-i18next';
 function Login() {
     const { t } = useTranslation();
     return (
-
-        <Stack className=" justify-content-center login-page py-5" direction="horizontal">
-            <div className="p-4 mb-2  test one">
-                <MyHeader title={t('p.login')}></MyHeader>
+        <Stack className="justify-content-center py-5 login-page full-height-page" direction="horizontal">
+            <div className="p-4 mb-2 basic-container">
+                <MyHeader title={t('p.loginHeader')}></MyHeader>
                 
                 <h6 className="display-6">Wpisz dane poniżej, aby się zalogować.</h6>
                 <input type="text" className="my-2"></input> <br></br>
@@ -17,13 +16,9 @@ function Login() {
                 <button className="my-2">somebutton</button>  <br></br>
             </div>
         </Stack>
-
-
     );
 
 
 }
 
 export default Login;
-
-//style od ramki do app.scss
