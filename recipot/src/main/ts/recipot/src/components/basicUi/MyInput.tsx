8291cd989bcec2955c4ca8783075c75a07bdb6ef
Monika Kordoń <37344587+mk1808@users.noreply.github.com>
2023-08-25 16:23:13 +0200

    feature #221: getting data and form validation with simple input

--- recipot/src/main/ts/recipot/src/components/basicUi/MyInput.tsx
+++ recipot/src/main/ts/recipot/src/components/basicUi/MyInput.tsx
@@ -1,5 +1,7 @@
 import Form from 'react-bootstrap/Form';
 import { initFcn } from '../../utils/ObjectUtils';
+import { useContext, useEffect, useState } from "react";
+import { Button } from 'react-bootstrap';
 
 function MyInput({
     name = "inputName",
@@ -7,8 +9,13 @@ function MyInput({
     type = "text",
     placeholder = "",
     disabled = false,
+
+    defaultValue = "",
+    required = false,
     onChange = initFcn<any>(),
-    defaultValue = ""
+    isValid,
+    value
+
 }: {
     name: string,
     label?: string,
@@ -16,20 +23,54 @@ function MyInput({
     placeholder?: string,
     disabled?: boolean,
     onChange?: Function,
-    defaultValue?: string
+    defaultValue?: string,
+    required?: boolean,
+    isValid?: boolean,
+    value?: string
 }) {
+    const [inputValue, setInputValue] = useState(defaultValue)
+
+    useEffect(() => { onChange(inputValue) }, [inputValue])
 
     function onChangeCallback(event: any) {
-        onChange(event.target.value)
+        setInputValue(event.target.value)
     }
+    const [validated, setValidated] = useState(false);
+    const handleSubmit = (event: any) => {
+        const form = event.currentTarget;
+        if (form.checkValidity() === false) {
+
+        }
+        event.preventDefault();
+        event.stopPropagation();
+        setValidated(true);
+    };
 
     return (
         <Form.Group className="mb-3" controlId={name}>
             {label && <Form.Label>{label}</Form.Label>}
-            <Form.Control type={type} placeholder={placeholder} disabled={disabled} onChange={onChangeCallback} defaultValue={defaultValue} />
+            <Form.Control
+                required={required}
+                type={type}
+                placeholder={placeholder}
+                disabled={disabled}
+                onChange={onChangeCallback}
+
+                defaultValue={defaultValue}
+                isValid={isValid} />
         </Form.Group>
     )
 }
 
 
 export default MyInput;
+
+/*
+<Form noValidate validated={validated} onSubmit={handleSubmit}>
+            <Form.Group className="mb-3" controlId={name}>
+                {label && <Form.Label>{label}</Form.Label>}
+                <Form.Control required type={type} placeholder={placeholder} disabled={disabled} onChange={onChangeCallback} />
+            </Form.Group>
+            <Button type="submit">Submit form</Button>
+        </Form>
+*/
