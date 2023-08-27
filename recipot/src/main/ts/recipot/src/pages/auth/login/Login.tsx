f0fbbcf8b6ba0190fcaccd7f3557c9493d3d3502
Monika Kordoń <37344587+mk1808@users.noreply.github.com>
2023-08-27 12:00:49 +0200

    feature #224: login fill

--- recipot/src/main/ts/recipot/src/pages/auth/login/Login.tsx
+++ recipot/src/main/ts/recipot/src/pages/auth/login/Login.tsx
@@ -1,10 +1,15 @@
-import { Stack } from 'react-bootstrap';
+import { Col, Row, Stack } from 'react-bootstrap';
 import './styles.scss';
 import MyHeader from '../../../components/basicUi/MyHeader';
 import { useTranslation } from 'react-i18next';
+import MyInput from '../../../components/basicUi/MyInput';
+import MyButton from '../../../components/basicUi/MyButton';
 
 function Login() {
     const { t } = useTranslation();
+    function handleSubmit() {
+
+    }
     return (
         <Stack className="justify-content-center py-5 login-page full-height-page" direction="horizontal">
             <div className="p-4 mb-2 basic-container basic-container-border">
@@ -18,9 +23,27 @@ function Login() {
         return (
             <div>
                 <h6 className="display-6">Wpisz dane poniżej, aby się zalogować.</h6>
-                <input type="text" className="my-2"></input> <br></br>
-                <input type="text" className="my-2"></input>  <br></br>
-                <button className="my-2">somebutton</button>  <br></br>
+                <Col className="main-column">
+                    <Row className="row">
+                        <MyInput
+                            isValid={true}
+                            name="test1"
+                            label={t('p.username')}
+                            placeholder="Input test 1"
+                            onChange={(value: string) => console.log(value)} />
+                        <MyInput
+                            type="password"
+                            isValid={true}
+                            name="test1"
+                            label={t('p.password')}
+                            placeholder="Input test 1"
+                            onChange={(value: string) => console.log(value)} />
+                    </Row>
+                </Col>
+
+                <MyButton.Primary onClick={handleSubmit} className="button-400">
+                    {t('p.login')}
+                </MyButton.Primary>
             </div>
         )
     }
