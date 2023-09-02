b00d3373bfb0b8fe0092f521e748a6e670d193d6
Monika Kordoń <37344587+mk1808@users.noreply.github.com>
2023-09-02 20:41:27 +0200

    feature #239: form management - recipe filter view dialog continuation

--- recipot/src/main/ts/recipot/src/pages/recipe/filter/dialogs/AddRecipeFilterDialog.tsx
+++ recipot/src/main/ts/recipot/src/pages/recipe/filter/dialogs/AddRecipeFilterDialog.tsx
@@ -1,23 +1,19 @@
-import { useReducer, useRef, useState } from "react";
+import { useRef } from "react";
 import CustomModal from "../../../../components/basicUi/CustomModal";
-import MyInput from "../../../../components/basicUi/MyInput";
-import { checkIfAllValid, checkInputValidity, getEmptyForm, getEmptyFormSave, getNewState, inputAttributes, preventFurtherAction } from "../../../../utils/FormInputUtils";
-import { Form } from "react-bootstrap";
-import { FormSave, MyForm } from "../../../../data/utilTypes";
+import { getEmptyFormSave } from "../../../../utils/FormInputUtils";
+import { FormSave } from "../../../../data/utilTypes";
 import { useTranslation } from "react-i18next";
 import AddRecipeFilterForm from "./AddRecipeFilterForm";
 
 function AddRecipeFilterDialog({ showModal, handleClose }: { showModal: boolean, handleClose: any }) {
     const { t } = useTranslation();
     const formSave: FormSave = getEmptyFormSave();
-    let formValue1: any;
     const form = useRef<any>();
-    let btn1: any;
-    let form1: any;
+    let formContent: any;
+
     formSave.onSubmit = function (formValue: any) {
-        formValue1 = formValue;
-        //zapytanie do backendu 
-        console.log("torest")
+        formContent = formValue;
+        console.log("zapytanie do backendu")
     }
     formSave.onSuccess = function () {
 
@@ -25,35 +21,13 @@ function AddRecipeFilterDialog({ showModal, handleClose }: { showModal: boolean,
     formSave.onError = function () {
 
     }
-    function afterCLick() {
-        console.log("afterCLick")
-    }
     async function myHandleSubmit() {
-        form.current.submifForm();
-
-        /*handleClose();
-        //   debugger;
-        console.log("form dial")
-        console.log(formValue1)
-        console.log("ciag dalszy")
-        console.log(btn1);
-        form1[1].click();
-
-        setTimeout(() => { console.log(formValue1) }, 3000)
-        //console.log(val)
-
-        //  btn1.click();
-*/
-    }
-
-    function chnaged(btn: any, form: any) {
-        btn1 = btn;
-        form1 = form;
+        form.current.submitForm();
     }
 
     return (
         <CustomModal shouldShow={showModal} handleClose={handleClose} title={'p.recipeFilterSave'}
-            handleSubmit={myHandleSubmit} >
+            handleSubmit={myHandleSubmit}>
             {renderContent()}
         </CustomModal>
     );
@@ -62,15 +36,10 @@ function AddRecipeFilterDialog({ showModal, handleClose }: { showModal: boolean,
         return (
             <>
                 <span>{t('p.recipeFilterSaveInfo')}</span>
-                <AddRecipeFilterForm formSave={formSave} chnaged={chnaged} ref={form}></AddRecipeFilterForm>
+                <AddRecipeFilterForm formSave={formSave} ref={form}></AddRecipeFilterForm>
             </>
         )
     }
-
-
-
-
-
 }
 
 export default AddRecipeFilterDialog;
--- recipot/src/main/ts/recipot/src/pages/recipe/filter/dialogs/AddRecipeFilterForm.tsx
+++ recipot/src/main/ts/recipot/src/pages/recipe/filter/dialogs/AddRecipeFilterForm.tsx
@@ -1,26 +1,25 @@
-import { useReducer, useRef, useState, useEffect, useImperativeHandle, forwardRef } from "react";
-import CustomModal from "../../../../components/basicUi/CustomModal";
+import { useReducer, useRef, useImperativeHandle, forwardRef } from "react";
 import MyInput from "../../../../components/basicUi/MyInput";
 import { checkIfAllValid, checkInputValidity, getEmptyForm, getEmptyFormSave, getNewState, inputAttributes, preventFurtherAction } from "../../../../utils/FormInputUtils";
 import { Form } from "react-bootstrap";
 import { FormSave, MyForm } from "../../../../data/utilTypes";
 import { useTranslation } from "react-i18next";
-import MyButton from "../../../../components/basicUi/MyButton";
 
-function AddRecipeFilterForm({ formSave, chnaged }: { formSave: FormSave, chnaged: any }, ref:any) {
+function AddRecipeFilterForm({ formSave }: { formSave: FormSave }, ref: any) {
     const { t } = useTranslation();
     const [myForm, dispatchForm]: [MyForm, Function] = useReducer(formReducer, getEmptyForm());
     const form = useRef<any>();
+
     useImperativeHandle(ref, () => ({
-        submifForm() {
+        submitForm() {
             handleSubmit();
         }
-      }));
+    }));
 
     function handleSubmit() {
-        const fakeEvent= {currentTarget:form.current}
+        const submitFormEvent = { currentTarget: form.current }
 
-        if (checkIfAllValid(fakeEvent, myForm)) {
+        if (checkIfAllValid(submitFormEvent, myForm)) {
             formSave.onSubmit(myForm.formValue);
             console.log('valid')
         } else {
@@ -32,19 +31,14 @@ function AddRecipeFilterForm({ formSave, chnaged }: { formSave: FormSave, chnage
         return getNewState(state, action, action.value, checkInputValidity);
     };
 
-    return (<>{renderForm()}</>)
-
-    function renderForm() {
-        return (
-            <Form id='my-form' noValidate validated={true} className="mt-3 text-start" ref={form}>
-                {renderFilterNameInput()}
-            </Form>
-        )
-    }
-
+    return (
+        <Form id='my-form' noValidate validated={true} className="mt-3 text-start" ref={form}>
+            {renderFilterNameInput()}
+        </Form>
+    )
 
     function renderFilterNameInput() {
-        return ( 
+        return (
             <MyInput
                 {...inputAttributes("newFilterName", myForm, dispatchForm)}
                 placeholder={t('p.newFilterNameInput')}
@@ -53,7 +47,6 @@ function AddRecipeFilterForm({ formSave, chnaged }: { formSave: FormSave, chnage
             />
         )
     }
-
 }
 
 export default forwardRef(AddRecipeFilterForm);
