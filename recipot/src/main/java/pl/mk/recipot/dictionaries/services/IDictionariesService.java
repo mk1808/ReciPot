391b666b41da5fde27c12548b13f3a2cff61fb27
Marek Czopor <marqos111222@gmail.com>
2023-09-02 17:44:00 +0200

    feature #276: add missing endpoints

--- recipot/src/main/java/pl/mk/recipot/dictionaries/services/IDictionariesService.java
+++ recipot/src/main/java/pl/mk/recipot/dictionaries/services/IDictionariesService.java
@@ -2,6 +2,8 @@ package pl.mk.recipot.dictionaries.services;
 
 import java.util.List;
 
+import pl.mk.recipot.commons.enums.RecipeAccessType;
+import pl.mk.recipot.commons.enums.RecipeAmountOfDishes;
 import pl.mk.recipot.commons.enums.RecipeDifficulty;
 import pl.mk.recipot.commons.enums.RecipeRequiredEffort;
 import pl.mk.recipot.dictionaries.dtos.CategoryDto;
@@ -13,4 +15,8 @@ public interface IDictionariesService {
 	List<RecipeRequiredEffort> getAllRequiredEfforts();
 
 	List<RecipeDifficulty> getAllDifficulties();
+
+	List<RecipeAccessType> getAllAccessTypes();
+
+	List<RecipeAmountOfDishes> getAllAmountOfDishes();
 }
