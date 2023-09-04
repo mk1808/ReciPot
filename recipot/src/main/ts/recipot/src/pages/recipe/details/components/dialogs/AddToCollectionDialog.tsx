039c5790ef773ba98894a92b7fd5d2e6adc47f85
Monika Kordoń <37344587+mk1808@users.noreply.github.com>
2023-09-04 17:26:14 +0200

    feature #239: recipe details - fill add recipe to collection form

--- recipot/src/main/ts/recipot/src/pages/recipe/details/components/dialogs/AddToCollectionDialog.tsx
+++ recipot/src/main/ts/recipot/src/pages/recipe/details/components/dialogs/AddToCollectionDialog.tsx
@@ -1,20 +1,41 @@
-import { useState } from "react";
+import { useRef, useState } from "react";
 import CustomModal from "../../../../../components/basicUi/CustomModal";
+import AddToCollectionForm from "./AddToCollectionForm";
+import { useTranslation } from "react-i18next";
+import { getEmptyFormSave } from "../../../../../utils/FormInputUtils";
+import { FormSave } from "../../../../../data/utilTypes";
 
 
 function AddToCollectionDialog({ showModal, handleClose  }: { showModal: boolean, handleClose: any}) {
-    function myHandleSubmit() {
-        handleClose();
-        console.log("ciag dalszy")
+    const { t } = useTranslation();
+    const formSave: FormSave = getEmptyFormSave();
+    const form = useRef<any>();
+    let formContent: any;
+
+    formSave.onSubmit = function (formValue: any) {
+        formContent = formValue;
+        console.log("zapytanie do backendu")
+        console.log(formContent);
+    }
+    formSave.onSuccess = function () {
+
+    }
+    formSave.onError = function () {
+
+    }
+    async function myHandleSubmit() {
+        form.current.submitForm();
     }
     return (
-        <CustomModal shouldShow={showModal} handleClose={handleClose} handleSubmit={myHandleSubmit}>
+        <CustomModal shouldShow={showModal} handleClose={handleClose} handleSubmit={myHandleSubmit} title={t("p.addingToCollection")}>
             {renderContent()}
         </CustomModal>
     );
 
     function renderContent() {
-        return (<>content AddToCollectionDialog</>)
+        return (<>
+            <AddToCollectionForm formSave={formSave} ref={form}></AddToCollectionForm>
+        </>)
     }
 
 }
