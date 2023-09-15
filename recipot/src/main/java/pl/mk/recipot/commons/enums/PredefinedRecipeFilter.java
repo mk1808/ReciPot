daf0cc7f7030de334123a474f5f06982f38b9159
Monika Kordoń <37344587+mk1808@users.noreply.github.com>
2023-09-15 23:42:40 +0200

    feature #18: refactor

--- recipot/src/main/java/pl/mk/recipot/commons/enums/PredefinedRecipeFilter.java
+++ recipot/src/main/java/pl/mk/recipot/commons/enums/PredefinedRecipeFilter.java
@@ -8,18 +8,7 @@ import pl.mk.recipot.commons.dtos.SearchOrderDto;
 
 public enum PredefinedRecipeFilter {
 
-	NEWEST("Newest",
-			RecipeSearchDto.builder()
-					.dataOption("ALL")
-					.searchCriteriaList(Arrays.asList(getPublicPartOfPredefinedFilter()))
-					.searchOrder(getDescSortOfField("created"))
-					.build()),
-	POPULAR("Popular",
-			RecipeSearchDto.builder()
-					.dataOption("ALL")
-					.searchCriteriaList(Arrays.asList(getPublicPartOfPredefinedFilter()))
-					.searchOrder(getDescSortOfField("ratingsCount"))
-					.build());
+	NEWEST("Newest", getForNewest()), POPULAR("Popular", getForPopular());
 
 	private String name;
 	private String jsonValue;
@@ -41,9 +30,25 @@ public enum PredefinedRecipeFilter {
 	private static SearchCriteriaDto getPublicPartOfPredefinedFilter() {
 		return SearchCriteriaDto.builder().filterKey("accessType").value("PUBLIC").operation("eq").build();
 	}
-	
+
 	private static SearchOrderDto getDescSortOfField(String fieldName) {
 		return SearchOrderDto.builder().fieldName(fieldName).order(SearchOrder.DESC).build();
+	}
+
+	private static RecipeSearchDto getForNewest() {
+		return RecipeSearchDto.builder()
+				.dataOption("ALL")
+				.searchCriteriaList(Arrays.asList(getPublicPartOfPredefinedFilter()))
+				.searchOrder(getDescSortOfField("created"))
+				.build();
+	}
+
+	private static RecipeSearchDto getForPopular() {
+		return RecipeSearchDto.builder()
+				.dataOption("ALL")
+				.searchCriteriaList(Arrays.asList(getPublicPartOfPredefinedFilter()))
+				.searchOrder(getDescSortOfField("ratingsCount"))
+				.build();
 	}
 
 }
