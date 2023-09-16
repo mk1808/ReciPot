2e269fa0379d50c42130fd69609fd92daf98001d
Monika Kordoń <37344587+mk1808@users.noreply.github.com>
2023-09-16 15:39:55 +0200

    feature #317: icons with info

--- recipot/src/main/ts/recipot/src/pages/recipe/details/components/BasicInfo.tsx
+++ recipot/src/main/ts/recipot/src/pages/recipe/details/components/BasicInfo.tsx
@@ -1,4 +1,4 @@
-import { Col, Row } from "react-bootstrap";
+import { Col, Row, Tooltip } from "react-bootstrap";
 import { useTranslation } from "react-i18next";
 import { Recipe } from "../../../../data/types";
 import MyButton from "../../../../components/basicUi/MyButton";
@@ -8,8 +8,13 @@ import AddToCollectionDialog from "./dialogs/AddToCollectionDialog";
 import ShareRecipeDialog from "./dialogs/ShareRecipeDialog";
 import ChangeVisibilityDialog from "./dialogs/ChangeVisibilityDialog";
 import DeleteRecipeDialog from "./dialogs/DeleteRecipeDialog";
+import { MdAccessTime, MdWork } from "react-icons/md";
+import { GiCookingPot } from "react-icons/gi";
+import { VscTools } from "react-icons/vsc";
+import Info from "../../../../components/basicUi/Info";
 
 function BasicInfo({ recipe }: { recipe: Recipe }) {
+    const { t } = useTranslation();
     const [showModalAddToCollection, setShowModalAddToCollection] = useState(false);
     const [showModalShare, setShowModalShare] = useState(false);
     const [showModalDelete, setShowModalDelete] = useState(false);
@@ -19,19 +24,49 @@ function BasicInfo({ recipe }: { recipe: Recipe }) {
             <div className="my-4">
                 {recipe.description}
             </div>
-            <Row>
+
+            <Row className="mb-5">
                 <Col className="border-right">
-                    <div className="test"></div>
+
+                    <Row className="align-center icon-info">
+                        <Col>
+
+                            <Info value={t('p.privateNoteInfo')} className="icon" renderIcon={(className: string) => <MdAccessTime className={className} />} /><br />
+                            <strong> Czas</strong> <br />
+                            30 min
+                        </Col>
+                        <Col>
+                            <Info value={t('p.privateNoteInfo')} className="icon" renderIcon={(className: string) => <GiCookingPot className={className} />} /><br />
+                            <strong> Ilość naczyń</strong> <br />
+                            duża
+                        </Col>
+                        <Col>
+                            <Info value={t('p.privateNoteInfo')} className="icon" renderIcon={(className: string) => <MdWork className={className} />} /><br />
+                            <strong>   Pracochłonność</strong> <br />
+                            średnia
+                        </Col>
+                        <Col>
+                            <Info value={t('p.privateNoteInfo')} className="icon" renderIcon={(className: string) => <VscTools className={className} />} /><br />
+                            <strong>   Skomplikowanie</strong><br />
+                            średnie
+
+                        </Col>
+                    </Row>
+
                 </Col>
+
+            </Row>
+            <Row className="align-center">
                 <Col className="border-left">
                     <div className="test">
-                        <MyButton.Primary onClick={() => setShowModalAddToCollection(true)}>Dodaj do kolekcji</MyButton.Primary>
-                        <MyButton.Primary onClick={() => setShowModalShare(true)}>Udostępnij</MyButton.Primary>
-                        <MyButton.Primary onClick={() => setShowModalDelete(true)}>Usuń przepis</MyButton.Primary>
-                        <MyButton.Primary onClick={() => setShowModalChangeVisibility(true)}>Zmień widoczność</MyButton.Primary>
+                        <MyButton.Primary onClick={() => setShowModalAddToCollection(true)} className="action-btn">Dodaj do kolekcji</MyButton.Primary>
+                        <MyButton.Primary onClick={() => setShowModalShare(true)} className="action-btn">Udostępnij</MyButton.Primary>
+                        <MyButton.Primary onClick={() => setShowModalDelete(true)} className="action-btn">Usuń przepis</MyButton.Primary>
+                        <MyButton.Primary onClick={() => setShowModalChangeVisibility(true)} className="action-btn" >Zmień widoczność</MyButton.Primary>
                         {renderDialogs()}
                     </div>
                 </Col>
+
             </Row>
         </div>
     )
@@ -42,7 +77,7 @@ function BasicInfo({ recipe }: { recipe: Recipe }) {
                 <ShareRecipeDialog showModal={showModalShare} handleClose={() => setShowModalShare(false)}></ShareRecipeDialog>
                 <DeleteRecipeDialog showModal={showModalDelete} handleClose={() => setShowModalDelete(false)} data={recipe}></DeleteRecipeDialog>
                 <ChangeVisibilityDialog showModal={showModalChangeVisibility} handleClose={() => setShowModalChangeVisibility(false)} data={recipe}></ChangeVisibilityDialog>
-                
+
             </>
         )
     }
