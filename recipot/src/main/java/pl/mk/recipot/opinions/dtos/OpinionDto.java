a622b65476cfb79735cf995b3bab560432ceda1a
Marek Czopor <marqos111222@gmail.com>
2023-09-17 17:14:48 +0200

    bug #328: add opinionDto created date

--- recipot/src/main/java/pl/mk/recipot/opinions/dtos/OpinionDto.java
+++ recipot/src/main/java/pl/mk/recipot/opinions/dtos/OpinionDto.java
@@ -1,5 +1,7 @@
 package pl.mk.recipot.opinions.dtos;
 
+import java.util.Date;
+
 import lombok.Builder;
 
 @Builder
@@ -8,13 +10,15 @@ public class OpinionDto {
 	public String authorAvatarImageSrc;
 	public String comment;
 	public Double rating;
+	public Date created;
 
-	public OpinionDto(String authorLogin, String authorAvatarImageSrc, String comment, Double rating) {
+	public OpinionDto(String authorLogin, String authorAvatarImageSrc, String comment, Double rating, Date created) {
 		super();
 		this.authorLogin = authorLogin;
 		this.authorAvatarImageSrc = authorAvatarImageSrc;
 		this.comment = comment;
 		this.rating = rating;
+		this.created = created;
 	}
 
 }
