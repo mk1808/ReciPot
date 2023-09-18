6ba1d435ec78a3604747b2948d780d052a50932d
Monika Kordoń <37344587+mk1808@users.noreply.github.com>
2023-09-18 23:41:46 +0200

    feature #252: getting recipes of the same category

--- recipot/src/main/ts/recipot/src/pages/recipe/details/RecipeDetails.tsx
+++ recipot/src/main/ts/recipot/src/pages/recipe/details/RecipeDetails.tsx
@@ -9,7 +9,7 @@ import IngredientList from "./components/IngredientsList";
 import Steps from "./components/Steps";
 import PrivateNote from "./components/PrivateNote";
 import Comments from "./components/Comments";
-import { OpinionDto, PrivateNote as PrivateNoteT, Recipe } from "../../../data/types";
+import { OpinionDto, PrivateNote as PrivateNoteT, Recipe, RecipeSearchDto } from "../../../data/types";
 import { initAs } from "../../../utils/ObjectUtils";
 import { useEffect, useState } from "react";
 import BreadCrumbs from "./components/BreadCrumbs";
@@ -28,11 +28,13 @@ import privateNotesApi from "../../../api/PrivateNotes";
 import StarSelectInput from "../../../components/basicUi/StarSelectInput";
 import { inputAttributes } from "../../../utils/FormInputUtils";
 import Rating from "./components/Rating";
+import { buildRecipeSearchDto } from "../../../utils/RecipeSearchUtils";
 
 function RecipeDetails() {
     const { t } = useTranslation();
     const params = useParams();
     const [recipe, setRecipe] = useState<any | Recipe>(initAs());
+    const [otherRecipes, setOtherRecipes] = useState<Recipe[]>([]);
     const [opinions, setOpinions] = useState<any | OpinionDto[]>([]);
     const [note, setNote] = useState<any | PrivateNoteT>(initAs());
     const [isLoaded, setIsLoaded] = useState<boolean>(false);
@@ -40,16 +42,17 @@ function RecipeDetails() {
 
     useEffect(() => {
         let id: string = params.id ?? "";
-        let b = {}
         console.log(params)
-        recipesApi.getRecipe(id, (response) => { setRecipe(response.value); setIsLoaded(true); })
+        recipesApi.getRecipe(id, (response) => {
+            setRecipe(response.value);
+            setIsLoaded(true);
+            const filter: RecipeSearchDto = buildRecipeSearchDto({ categories: response.value.categories, recipesSort: { fieldName: "created", order: "DESC" } });
+            recipesApi.search(filter, { pageNum: 0, pageSize: 10 }, (response) => { setOtherRecipes(response.value.content) });
+        })
         getOpinions(id);
         privateNotesApi.getPrivateNoteByRecipeId(id, (response) => { setNote(response.value); setIsNoteLoaded(true) });
-
     }, [])
     function getOpinions(id: string) { opinionsApi.getRecipeOpinions(id, (response) => { setOpinions(response.value) }) }
-
-    const recipes = [{}, {}, {}, {}, {}, {}, {}]
 
     return (
         <div className='m-2 recipe-details-page'>
@@ -67,7 +70,7 @@ function RecipeDetails() {
                 </Col>
                 <Col md={2}>
                     <div className='h-100 basic-container-border p-3'>
-                        <OtherColumn />
+                        <OtherColumn recipes={otherRecipes} />
                     </div>
                 </Col>
                 <Col md={1}></Col>
@@ -86,9 +89,6 @@ function RecipeDetails() {
                     <Col md={1}></Col>
                     <Col md={6}><Rating recipe={recipe}></Rating></Col>
                 </Row>
-                
-              
-
                 <BasicInfo recipe={recipe} />
                 <hr />
                 <IngredientList recipe={recipe} />
