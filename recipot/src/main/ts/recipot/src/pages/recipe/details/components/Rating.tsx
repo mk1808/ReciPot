12853e7267427b07d61ea8e5f8f283b626c53f36
Monika Kordoń <37344587+mk1808@users.noreply.github.com>
2023-09-18 20:52:49 +0200

    feature #252: displaying average rating

--- recipot/src/main/ts/recipot/src/pages/recipe/details/components/Rating.tsx
+++ recipot/src/main/ts/recipot/src/pages/recipe/details/components/Rating.tsx
@@ -1,23 +1,25 @@
 import { Col, Row } from "react-bootstrap";
 import StarSelectInput from "../../../../components/basicUi/StarSelectInput";
 import { Recipe } from "../../../../data/types";
+import { roundToHalf } from "../../../../utils/MathUtils";
 
 function Rating({ recipe }: { recipe: Recipe }) {
     return (
-        <Row className="px-5 rating"><Col></Col>
-            <Col md={4}>
+        <Row className="px-5 rating" gap={1}>
+            <Col></Col>
+            <Col md={4} className="pe-0 star-col">
                 <StarSelectInput
                     required
                     disabled={true}
-                    defaultValue={3}
-                    name="star"
+                    defaultValue={roundToHalf(recipe.averageRating)}
+                    name=""
                     isValid={true}
-                    onChange={(value: string) => { }}
+                    onChange={(value: any) => { }}
                 />
             </Col>
 
-            <Col md={4} className="">
-                <span>5 / 5 (20 ocen)</span>
+            <Col md={5} className="ps-0">
+                <span>{recipe.averageRating} / 5 ({recipe.ratingsCount} ocen)</span>
             </Col>
         </Row>
     )
