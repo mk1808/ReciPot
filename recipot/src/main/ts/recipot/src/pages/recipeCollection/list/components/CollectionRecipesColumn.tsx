21dcb2c4fe493eea6b406c6cddc52340462cd664
Monika Kordoń <37344587+mk1808@users.noreply.github.com>
2023-09-21 01:46:17 +0200

    bug #358: fix go to recipe

--- recipot/src/main/ts/recipot/src/pages/recipeCollection/list/components/CollectionRecipesColumn.tsx
+++ recipot/src/main/ts/recipot/src/pages/recipeCollection/list/components/CollectionRecipesColumn.tsx
@@ -4,11 +4,14 @@ import { Stack } from "react-bootstrap";
 import { RecipeCollectionListContext } from "../context/RecipeCollectionListContext";
 import { useTranslation } from "react-i18next";
 import { Recipe } from "../../../../data/types";
+import { useNavigate } from "react-router-dom";
 
 
 function CollectionRecipesColumn() {
     const collectionsContext = useContext(RecipeCollectionListContext);
     const { t } = useTranslation();
+    const navigate = useNavigate();
+    const recipeCallback = (recipe: Recipe) => { navigate(`/recipes/${recipe.id}`) }
 
     return (
         <div>
@@ -22,7 +25,7 @@ function CollectionRecipesColumn() {
             <div key={pageId} id={pageId}>
                 <h3>-------------- {t('p.page')} {index + 1} --------------</h3>
                 <Stack direction="horizontal" className="flex-wrap justify-content-center" gap={3}>
-                    {recipes?.map(recipe => <RecipeCard key={recipe.id} recipe={recipe} recipeCallback={() => { }}></RecipeCard >)}
+                    {recipes?.map(recipe => <RecipeCard key={recipe.id} recipe={recipe} recipeCallback={recipeCallback}></RecipeCard >)}
                 </Stack>
             </div>
         );
