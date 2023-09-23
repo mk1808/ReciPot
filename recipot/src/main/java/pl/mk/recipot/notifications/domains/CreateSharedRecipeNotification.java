a2c760c9e8cce407fd3f4a810eed82e488415ac1
Marek Czopor <marqos111222@gmail.com>
2023-09-23 19:35:48 +0200

    bug #381: fix share recipe notification

--- recipot/src/main/java/pl/mk/recipot/notifications/domains/CreateSharedRecipeNotification.java
+++ recipot/src/main/java/pl/mk/recipot/notifications/domains/CreateSharedRecipeNotification.java
@@ -1,6 +1,7 @@
 package pl.mk.recipot.notifications.domains;
 
 import pl.mk.recipot.commons.enums.NotificationType;
+import pl.mk.recipot.commons.models.AppUser;
 import pl.mk.recipot.commons.models.SharedRecipe;
 
 public class CreateSharedRecipeNotification extends AbstractCreateRecipeNotification<SharedRecipe> {
@@ -12,5 +13,10 @@ public class CreateSharedRecipeNotification extends AbstractCreateRecipeNotifica
 	@Override
 	protected Object getValue(SharedRecipe sharedRecipe) {
 		return sharedRecipe.getComment();
+	}
+
+	@Override
+	protected AppUser getOwner(SharedRecipe sharedRecipe) {
+		return sharedRecipe.getReceiverUser();
 	}
 }
