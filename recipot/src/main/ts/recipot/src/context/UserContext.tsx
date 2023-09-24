640fa6159eb58a4cd89751952006c0bbd8313fb6
Marek Czopor <marqos111222@gmail.com>
2023-09-24 11:07:59 +0200

    bug #392: logout user when token inactive

--- recipot/src/main/ts/recipot/src/context/UserContext.tsx
+++ recipot/src/main/ts/recipot/src/context/UserContext.tsx
@@ -1,13 +1,16 @@
-import { createContext, useEffect, Context, useReducer, useContext } from "react";
+import { createContext, useEffect, Context, useReducer, useContext, useRef } from "react";
 import authApi from "../api/AuthApi";
 import { AppUser, Response } from "../data/types";
-import { showSuccessAlert } from "../utils/RestUtils";
+import { showErrorAlert, showSuccessAlert } from "../utils/RestUtils";
 import { useTranslation } from "react-i18next";
 import { AlertsDispatchContext } from "./AlertContext";
+import { ApiRequestSendManager } from "../utils/ApiRequestSendManager";
 
 export const UsersContext: Context<{ user?: AppUser }> = createContext({});
 
 export const UsersDispatchContext = createContext<Function>(() => { });
+
+const searchRequestManager = ApiRequestSendManager();
 
 export const UserContextProvider = ({ children }: any) => {
     const { t } = useTranslation();
@@ -16,20 +19,39 @@ export const UserContextProvider = ({ children }: any) => {
         usersReducer, null
     );
 
+    function refreshRequest() {
+        searchRequestManager.nextAndLock(() => {
+            authApi.whoAmI(onSuccessRefresh, onError)
+        })
+    }
+
     function onSuccessRefresh(response: Response<AppUser>) {
+        searchRequestManager.unlock();
         console.log(response.value);
         let action = { user: response.value, type: 'logged' }
         dispatch(action);
+        refreshUser();
     };
 
     function onError(response: any) {
-        console.log("error")
+        searchRequestManager.unlock();
+        if (user) {
+            showErrorAlert(t("p.userTokenTimeout"), alertsDispatchContext);
+        }
+        dispatch({ user: null, type: 'logged' });
     };
 
     function onSuccessLogout(response: Response<any>) {
         showSuccessAlert(t(response.message), alertsDispatchContext);
         console.log(response);
     };
+
+    function refreshUser() {
+        const intervalTime = 1000 * 10; //minute
+        setTimeout(() => {
+            dispatch({ type: 'refresh' })
+        }, intervalTime);
+    }
 
     function usersReducer(user: any, action: any) {
         switch (action.type) {
@@ -41,7 +63,7 @@ export const UserContextProvider = ({ children }: any) => {
                 return null;
             }
             case 'refresh': {
-                authApi.whoAmI(onSuccessRefresh, onError)
+                refreshRequest();
                 return user;
             }
             default: {
--- recipot/src/main/ts/recipot/src/pages/other/noAccess/NoAccess.tsx
+++ recipot/src/main/ts/recipot/src/pages/other/noAccess/NoAccess.tsx
@@ -1,9 +1,19 @@
 import { useTranslation } from "react-i18next";
+import MyButton from "../../../components/basicUi/MyButton";
+import { useNavigate } from "react-router-dom";
 
 function NoAccess() {
     const { t } = useTranslation();
+    const navigate = useNavigate();
     return (
-        <h1>{t('p.noAccess')}</h1>
+        <>
+            <h1>{t('p.noAccess')}</h1>
+            <br />
+
+            <MyButton.Primary className="button-400" onClick={() => navigate('/login')}>
+                {t('p.login')}
+            </MyButton.Primary>
+        </>
     );
 
 
