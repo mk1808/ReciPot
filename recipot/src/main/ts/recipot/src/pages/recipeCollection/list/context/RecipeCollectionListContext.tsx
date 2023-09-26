61667942de0055ed166bf78ea30dd8ac7890f606
Marek Czopor <marqos111222@gmail.com>
2023-09-26 19:29:03 +0200

    bug #415: add load next recipes page and load between pages on collections recipe page

--- recipot/src/main/ts/recipot/src/pages/recipeCollection/list/context/RecipeCollectionListContext.tsx
+++ recipot/src/main/ts/recipot/src/pages/recipeCollection/list/context/RecipeCollectionListContext.tsx
@@ -2,10 +2,14 @@ import { createContext, useEffect, Context, useReducer } from "react";
 import { Recipe, RecipeCollection, RecipeCollectionItem, Response } from "../../../../data/types";
 import recipeCollectionsApi from "../../../../api/RecipeCollectionsApi";
 import { ResponsePage } from "../../../../data/utilTypes";
+import { scrollIntoRecipesPage } from "../../../../utils/RecipeSearchUtils";
+import { ApiRequestSendManager } from "../../../../utils/ApiRequestSendManager";
 
 type contextStateModel = { collections?: RecipeCollection[], recipesInCollection?: Recipe[][], activeCollectionId?: string, currentPage?: { totalPages: number, number: number } };
 
 const RECIPES_PAGE_SIZE = 4;
+
+const searchRequestManager = ApiRequestSendManager();
 
 export const RecipeCollectionListContext: Context<contextStateModel> = createContext({});
 
@@ -18,14 +22,21 @@ export const RecipeCollectionListContextProvider = ({ children }: any) => {
         recipeCollectionsApi.getUserRecipeCollections((response) => dispatch({ type: 'setSavedCollectionsList', value: response.value }))
     }
 
-    function getCollectionRecipes(recipeCollectionId: string, pageNum: number, pageSize: number) {
-        recipeCollectionsApi.getRecipeCollectionRecipes(recipeCollectionId, { pageNum, pageSize }, onCollectionRecipesResponse);
+    function getCollectionRecipes(recipeCollectionId: string, pageNum: number, pageSize: number, onResponseCallback = onCollectionRecipesResponse) {
+        searchRequestManager.nextAndLock(() => {
+            recipeCollectionsApi.getRecipeCollectionRecipes(recipeCollectionId, { pageNum, pageSize }, onResponseCallback, searchRequestManager.unlock);
+        })
     }
 
     function onCollectionRecipesResponse(response: Response<ResponsePage<RecipeCollectionItem>>): any {
+        searchRequestManager.unlock();
         const responsePage: ResponsePage<any> = response.value
-        responsePage.content = responsePage.content.map((collectionItem: RecipeCollectionItem) => collectionItem.recipe);
+        responsePage.content = responsePage.content.map(mapRecipeCollectionItemToRecipe);
         dispatch({ type: 'onRecipesPageLoad', recipesPage: responsePage });
+    }
+
+    function mapRecipeCollectionItemToRecipe(recipeCollectionItem: RecipeCollectionItem): Recipe {
+        return recipeCollectionItem.recipe;
     }
 
     function selectDefaultCollection(contextState: contextStateModel, collections: RecipeCollection[]) {
@@ -35,6 +46,28 @@ export const RecipeCollectionListContextProvider = ({ children }: any) => {
                     .forEach((collection) => dispatch({ type: 'collectionSelect', activeCollectionId: collection.id }));
             })
         };
+    }
+
+    function loadBetweenPage(contextState: contextStateModel, nextPages: Recipe[][], nextPage: any) {
+        const page = nextPage.number - 1
+        if (page > 0 && typeof nextPages[page] === 'undefined') {
+            getCollectionRecipes(contextState.activeCollectionId || "", page, RECIPES_PAGE_SIZE, onGetBetweenRecipesByFilterResponse)
+            return;
+        }
+    }
+
+    function onGetBetweenRecipesByFilterResponse(response: Response<ResponsePage<RecipeCollectionItem>>) {
+        searchRequestManager.unlock();
+        dispatch({
+            type: 'onBetweenRecipePageLoad',
+            recipesPage: response.value
+        });
+    }
+
+    function focusOnRecipesPage(page?: number) {
+        setTimeout(() => {
+            scrollIntoRecipesPage(page || 0);
+        }, 400)
     }
 
     function collectionsReducer(contextState: contextStateModel, action: any): contextStateModel {
@@ -63,10 +96,21 @@ export const RecipeCollectionListContextProvider = ({ children }: any) => {
             case 'onRecipesPageLoad': {
                 const recipesInCollection = [...(contextState?.recipesInCollection || [])]
                 recipesInCollection[action.recipesPage.number] = action.recipesPage.content
+                loadBetweenPage(contextState, recipesInCollection, action.recipesPage)
                 return {
                     ...contextState,
                     recipesInCollection: recipesInCollection,
                     currentPage: action.recipesPage
+                };
+            }
+            case 'onBetweenRecipePageLoad': {
+                const recipesInCollection = [...(contextState?.recipesInCollection || [])];
+                recipesInCollection[action.recipesPage.number] = action.recipesPage.content.map(mapRecipeCollectionItemToRecipe);
+                loadBetweenPage(contextState, recipesInCollection, action.recipesPage);
+                focusOnRecipesPage(contextState.currentPage?.number);
+                return {
+                    ...contextState,
+                    recipesInCollection: recipesInCollection
                 };
             }
             case 'loadRecipesPage': {
