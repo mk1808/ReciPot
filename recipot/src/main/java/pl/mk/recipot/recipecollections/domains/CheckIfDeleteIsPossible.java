5413b5a3eb4f63a0bd027c5e3d9a9eea09b63421
Monika Kordoń <37344587+mk1808@users.noreply.github.com>
2023-09-28 14:55:29 +0200

    feature #394: delete recipe from collection fix endpoint

--- recipot/src/main/java/pl/mk/recipot/recipecollections/domains/CheckIfDeleteIsPossible.java
+++ recipot/src/main/java/pl/mk/recipot/recipecollections/domains/CheckIfDeleteIsPossible.java
@@ -1,12 +1,14 @@
 package pl.mk.recipot.recipecollections.domains;
 
+import pl.mk.recipot.commons.enums.DefaultRecipeCollections;
 import pl.mk.recipot.commons.exceptions.ConflictException;
 import pl.mk.recipot.commons.models.RecipeCollection;
 
 public class CheckIfDeleteIsPossible {
 
 	public void execute(RecipeCollection recipeCollection) {
-		if (!recipeCollection.isCanDelete()) {
+		if (!recipeCollection.isCanDelete()
+				&& !DefaultRecipeCollections.FAVOURITE.getName().equals(recipeCollection.getName())) {
 			throw new ConflictException("recipeCollections.error.defaultCollectionCannotBeDeleted");
 		}
 	}
