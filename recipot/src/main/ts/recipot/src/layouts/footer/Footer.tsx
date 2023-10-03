0fe84e6e0608b99886065ba0fc9c6cdbe5d9a7b3
Marek Czopor <marqos111222@gmail.com>
2023-10-03 22:43:02 +0200

    feature #496: change footer

--- recipot/src/main/ts/recipot/src/layouts/footer/Footer.tsx
+++ recipot/src/main/ts/recipot/src/layouts/footer/Footer.tsx
@@ -1,12 +1,56 @@
 import Card from 'react-bootstrap/Card';
+import { useTranslation } from 'react-i18next';
+import { FaGithub } from 'react-icons/fa6';
+import Tooltip from '../../components/basicUi/Tooltip';
+import { Stack } from 'react-bootstrap';
 
 function Footer() {
+  const { t } = useTranslation();
+
+  function getYear() {
+    return new Date().getFullYear();
+  }
+
   return (
     <Card>
-      <Card.Body>&#169;Recipot 2023
+      <Card.Body>
+        <Stack direction='horizontal' className='justify-content-center fs-5 my-2'>
+          {renderCopyright()}
+          {renderAuthors()}
+        </Stack >
       </Card.Body>
     </Card>
   );
+
+  function renderCopyright() {
+    return (
+      <div>
+        <span className='mx-1'>&#169;</span>
+        Recipot {getYear()}
+      </div>
+    )
+  }
+
+  function renderAuthors() {
+    return (
+      <Stack direction='horizontal' className='ms-3'>
+        <span className='mx-2'>{t('p.authors')}:</span>
+        {renderWithGithubAndTooltip('Monika Kordoń', 'https://github.com/mk1808')}
+        <span className='mx-2'>&</span>
+        {renderWithGithubAndTooltip('Marek Czopor', 'https://github.com/marqos12')}
+      </Stack>
+    )
+  }
+
+  function renderWithGithubAndTooltip(author: string, gitHubUrl: string) {
+    return (
+      <a href={gitHubUrl} target='_blank' rel='noreferrer'>
+        <Tooltip placement="top" title={t('p.githubProfile')}>
+          {author} <FaGithub />
+        </Tooltip>
+      </a>
+    )
+  }
 }
 
 export default Footer;
