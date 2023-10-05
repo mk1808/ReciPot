611239b6226d15e371df3ea14f5a7a22d36cdb50
Monika Kordoń <37344587+mk1808@users.noreply.github.com>
2023-10-05 23:04:48 +0200

    feature #505: correct loader display

--- recipot/src/main/ts/recipot/src/pages/recipe/filter/components/FilteredRecipesColumn.tsx
+++ recipot/src/main/ts/recipot/src/pages/recipe/filter/components/FilteredRecipesColumn.tsx
@@ -8,11 +8,13 @@ import { useNavigate } from "react-router-dom";
 import MyButton from "../../../../components/basicUi/MyButton";
 import { openInBackground } from "../../../../utils/NavigationUtils";
 import NoContent from "../../../../components/complex/NoContent";
+import MySpinner from "../../../../components/basicUi/MySpinner";
 
 
 function FilteredRecipesColumn() {
     const recipeFilterContext = useContext(RecipeFilterContext);
     const recipeFilterDispatchContext = useContext(RecipeFilterDispatchContext);
+    const isLoaded = recipeFilterContext.isLoaded;
     const { t } = useTranslation();
     const navigate = useNavigate();
     const recipeCallback = (recipe: Recipe, event: any) => openInBackground(`/recipes/${recipe.id}`, event, navigate);
@@ -23,7 +25,13 @@ function FilteredRecipesColumn() {
         })
     }
 
-    return (recipeFilterContext.currentPage?.totalElements || 0) > 0 ? renderContent() : renderNoData();
+    return (
+        <>
+            {!isLoaded && <MySpinner />}
+            {isLoaded && (recipeFilterContext.currentPage?.totalElements || 0 > 0 ? renderContent() : renderNoData())}
+
+        </>
+    )
 
     function renderContent() {
         return (
@@ -60,7 +68,7 @@ function FilteredRecipesColumn() {
 
     function renderNoData() {
         return (
-            <NoContent text={t('p.noElementsInSearch')}/>
+            <NoContent text={t('p.noElementsInSearch')} />
         );
     }
 }
