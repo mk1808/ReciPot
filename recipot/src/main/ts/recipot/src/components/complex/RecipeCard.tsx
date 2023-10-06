ed7c50b264b9a29ac5c7c4e10993184f6e89f0a9
Monika Kordoń <37344587+mk1808@users.noreply.github.com>
2023-10-06 22:39:32 +0200

    bug #534: change size of text and correct categories display on recipe card circle, refactor

--- recipot/src/main/ts/recipot/src/components/complex/RecipeCard.tsx
+++ recipot/src/main/ts/recipot/src/components/complex/RecipeCard.tsx
@@ -5,7 +5,7 @@ import MyButton from '../basicUi/MyButton';
 import { useTranslation } from 'react-i18next';
 import { Recipe } from '../../data/types';
 import { initFcn } from '../../utils/ObjectUtils';
-import { renderRating } from './RecipeCardCommonElements';
+import { renderCategories, renderMore, renderRating } from './RecipeCardCommonElements';
 import { getShorterText } from '../../utils/TextUtils';
 import HashTagList from '../basicUi/HashTagList';
 import { onImageLoadError } from "../../utils/RestUtils";
@@ -31,18 +31,6 @@ function RecipeCard({
         goToFilters({ hashTags: [{ label: hashTag.name, value: hashTag }] }, navigate);
     }
 
-    function getMoreNumber(list?: any[]) {
-        const listLength = list?.length || 1;
-        return listLength - 1;
-    }
-
-    function getMoreList(list?: any[]) {
-        if (list?.length && list.length > 0) {
-            return list.slice(1, list.length).map(element => element.name).join(", ");
-        }
-        return "";
-    }
-
     return (
         <Card className={`recipe-card mb-2 ${(className || "me-2")}`} ref={ref}>
             <Card.Img variant="top" src={recipe.image} height={180} onError={onImageLoadError} />
@@ -65,12 +53,7 @@ function RecipeCard({
         return (
             <Row>
                 <Col>
-                    <Stack direction="horizontal">
-                        <h6>
-                            {recipe.categories.map(category => category.name)[0]}
-                        </h6>
-                        {renderMore(recipe.categories)}
-                    </Stack>
+                    {renderCategories(recipe)}
                 </Col>
                 {additionalFunctionElement && <Col md={3}>{additionalFunctionElement}</Col>}
             </Row>
@@ -84,15 +67,6 @@ function RecipeCard({
                 {renderMore(recipe.hashTags)}
             </Stack>
         )
-    };
-
-    function renderMore(list?: any[]) {
-        const elementsNumber = getMoreNumber(list);
-        return elementsNumber > 0 && (
-            <Tooltip placement={"bottom"} title={getMoreList(list)}>
-                <h6 className="ms-2 cursor-pointer">{`(+${elementsNumber})`}</h6>
-            </Tooltip>
-        );
     };
 }
 
--- recipot/src/main/ts/recipot/src/components/complex/RecipeCardCommonElements.tsx
+++ recipot/src/main/ts/recipot/src/components/complex/RecipeCardCommonElements.tsx
@@ -1,5 +1,7 @@
 import { BsStarFill } from 'react-icons/bs';
 import { Recipe } from '../../data/types';
+import Tooltip from '../basicUi/Tooltip';
+import { Stack } from 'react-bootstrap';
 
 export function renderRating(recipe: Recipe, text: string) {
     return (
@@ -10,3 +12,35 @@ export function renderRating(recipe: Recipe, text: string) {
         </div>
     )
 };
+
+export function renderMore(list?: any[]) {
+
+    function getMoreNumber(list?: any[]) {
+        const listLength = list?.length || 1;
+        return listLength - 1;
+    }
+
+    function getMoreList(list?: any[]) {
+        if (list?.length && list.length > 0) {
+            return list.slice(1, list.length).map(element => element.name).join(", ");
+        }
+        return "";
+    }
+    const elementsNumber = getMoreNumber(list);
+    return elementsNumber > 0 && (
+        <Tooltip placement={"bottom"} title={getMoreList(list)}>
+            <h6 className="ms-2 cursor-pointer">{`(+${elementsNumber})`}</h6>
+        </Tooltip>
+    );
+};
+
+export function renderCategories(recipe: Recipe) {
+    return (
+        <Stack direction="horizontal">
+            <h6>
+                {recipe.categories.map(category => category.name)[0]}
+            </h6>
+            {renderMore(recipe.categories)}
+        </Stack>
+    )
+}
