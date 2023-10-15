4b9230e9da601ef22b66ec32a77109aeb6dc2ac3
Monika Kordoń <37344587+mk1808@users.noreply.github.com>
2023-10-15 10:12:56 +0200

    feature #479: allow to save file url in recipe

--- recipot/src/main/java/pl/mk/recipot/files/services/FilesService.java
+++ recipot/src/main/java/pl/mk/recipot/files/services/FilesService.java
@@ -1,11 +1,8 @@
 package pl.mk.recipot.files.services;
 
-import java.io.IOException;
 import java.util.UUID;
 
-import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
-import org.springframework.util.StringUtils;
 import org.springframework.web.multipart.MultipartFile;
 
 import pl.mk.recipot.commons.models.File;
