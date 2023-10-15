b4c309fc87ff77dec584343a8f229d36cd8a293f
Monika Kordoń <37344587+mk1808@users.noreply.github.com>
2023-10-15 09:28:13 +0200

    feature #479: correct url

--- recipot/src/main/ts/recipot/src/api/FilesApi.ts
+++ recipot/src/main/ts/recipot/src/api/FilesApi.ts
@@ -5,7 +5,7 @@ function FilesApi() {
     const PREFIX = '/files';
 
     const saveFile = (file: any, onSuccess: (response: Response<any>) => any, onError: any) => {
-        restClient.postFile(`${PREFIX}/save`, file, onSuccess, onError);
+        restClient.postFile(`${PREFIX}`, file, onSuccess, onError);
     }
 
     return { saveFile }
